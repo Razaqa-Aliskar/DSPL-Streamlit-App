d00c936ac4463e14a3f5f3c672bcde803fe8b8c5
Razaqa-Aliskar <razaqa.20221923@iit.ac.lk>
2025-04-28 13:27:20 +0530

    setting up with the imported libraries

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 import streamlit as st
+import pandas as pd
 st.write ("razaqaa")
+
+df = pd.read_csv('poverty_lka_cleaned.csv')
+
+# Add a title to the dashboard
+st.title('Interactive Dashboard')
+
+# Add a subheader
+st.subheader('Exploring the Data')
+
+# Display the data
+st.write(df)
